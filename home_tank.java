9f2fdef95203259c2a30afccb5dced762cead915
JoeLancey <j.odtohan.557940@umindanao.edu.ph>
2025-08-26 13:23:53 +0800

    Add files via upload

---
+++ home_tank.java
@@ -0,0 +1,32 @@
+package JAV_PACKAGE;
+
+import javax.swing.JOptionPane;
+
+public class  home_tank extends allTanks {
+	
+    public home_tank(int capacity, String type) {
+        super(capacity, type);
+        
+    }
+
+    @Override
+    public void fillTank(int liters) {
+        currentLevel = Math.min(currentLevel + liters, capacity);
+    }
+
+    @Override
+    public void useWater(int liters) {
+        currentLevel = Math.max(currentLevel - liters, 0);
+    }
+
+    @Override
+    public void checkStatus() {
+        if (currentLevel == 0) {
+            JOptionPane.showMessageDialog(null, type + " is EMPTY.");
+        } else if (currentLevel == capacity) {
+            JOptionPane.showMessageDialog(null, type + " is FULL.");
+        } else {
+            JOptionPane.showMessageDialog(null, type + " is IN USE. Current level: " + currentLevel + " liters");
+        }
+    }
+}
